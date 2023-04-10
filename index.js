2507ffc997464bd2ac7c95d19533b7e914a0b9b4
danielkuchu <117779534+danielkuchu@users.noreply.github.com>
2023-04-10 16:30:14 -0700

    js added

--- index.js
+++ index.js
@@ -3,4 +3,38 @@
 console.log("JS running ma boi");
 "use strict";
 
+const darkButton = document.getElementById("darkButton");
+const lightButton = document.getElementById("lightButton");
+const navBar = document.getElementById("navBar");
+const main = document.getElementById("container");
+const body = document.querySelector("body");
+const cloud = document.getElementById("cloud");
+
+
+darkButton.addEventListener("click", function(){
+
+body.style.backgroundImage = "url('images/grass2.png')";
+main.style.backgroundImage= "url('images/background2.png')";
+darkButton.className="hide";
+lightButton.className="show";
+navBar.style.color="red";
+
+if (backgroundImage == "url('images/grass2.png')") {
+
+    cloudg
+
+}
+
+});
+
+lightButton.addEventListener("click", function(){
+
+    body.style.backgroundImage = "url('images/grass.png')";
+    main.style.backgroundImage= "url('images/background1.png')";
+    darkButton.className="show";
+    lightButton.className="hide";
+    
+    
+    });
+
 })();
